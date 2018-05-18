b1752f7db5a7a05835b70aca429df7e807b98952
Anton Vasilev <bysslaev@gmail.com>
2018-05-18 03:42:38 +0300

    plot fit trend

--- crossing_over.m
+++ crossing_over.m
@@ -3,3 +3,6 @@ n = size(parent1, 2);
 index = randi([1,n]);
 parent1(index) = parent2(index);
 chromosome = parent1;
+if (randi([1,10]) == 5)
+    chromosome(randi([1,n])) = 1;
+end
--- initPopulation.m
+++ initPopulation.m
@@ -2,5 +2,5 @@ function result = initPopulation(n, size_chromosome)
 result = zeros(n,size_chromosome);
 for i = 1:n
 %     chromosome = chromosomeEncode(abs(rand() * 10), size_chromosome);
-    result(i, :) = mod(round(rand(1, size_chromosome) * 10), 2);
+    result(i, :) = randi([0,1],1,size_chromosome);
 end
